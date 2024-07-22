2718110460374777b3fbeb3198f90a0df4782041
Kacper <kacper.gajko1@gmail.com>
2024-07-22 16:30:04 +0200

    create session id cookie if user is valid

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,43 @@
 import "./style.scss";
+import { createClient } from "./utils/supabase/server";
+import { cookies } from "next/headers";
+
+async function Login(form: FormData) {
+	"use server";
+	const supabase = createClient();
+
+	const name = form.get("name");
+	const password = form.get("password");
+
+	const { data: user, error } = await supabase
+		.from("users")
+		.select()
+		.eq("name", name)
+		.eq("password", password);
+
+	if (user?.length != 0) {
+		const uuid = crypto.randomUUID();
+		cookies().set("session_id", uuid);
+	} else {
+		console.log("nuh uh");
+	}
+}
 
 export default function Home() {
 	return (
 		<main>
-			<form>
+			<form action={Login}>
 				<label>Name</label>
-				<input type='text' />
+				<input
+					type='text'
+					name='name'
+				/>
 
 				<label>Password</label>
-				<input type='password' />
+				<input
+					type='password'
+					name='password'
+				/>
 
 				<button>
 					<img src='/login.png' />
