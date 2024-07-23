30a1d8bdf70b3b68b2a58d9d142099f515aca6d4
Kacper <kacper.gajko1@gmail.com>
2024-07-23 10:41:50 +0200

    grid tiles template

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -3,7 +3,32 @@ import "./style.scss";
 export default function Dashboard() {
 	return (
 		<div className='tiles'>
-			<div className='grid'></div>
+			<div className='grid'>
+				<div className='upper-grid-tile'>
+					<img src='uptime.png' />
+					<div className='tile-title'>Uptime</div>
+				</div>
+				<div className='upper-grid-tile'>
+					<img src='cpu.png' />
+					<div className='tile-title'>CPU</div>
+				</div>
+				<div className='upper-grid-tile'>
+					<img src='ram.png' />
+					<div className='tile-title'>RAM</div>
+				</div>
+				<div className='upper-grid-tile'>
+					<img src='hdd.png' />
+					<div className='tile-title'>Storage</div>
+				</div>
+				<div className='middle-grid-tile'></div>
+				<div className='middle-grid-tile'></div>
+				<div className='middle-grid-tile'></div>
+				<div className='middle-grid-tile'></div>
+				<div className='bottom-grid-tile'></div>
+				<div className='bottom-grid-tile'></div>
+				<div className='bottom-grid-tile'></div>
+				<div className='bottom-grid-tile'></div>
+			</div>
 		</div>
 	);
 }
