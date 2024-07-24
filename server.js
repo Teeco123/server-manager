b517b12feb6efc9bc895edfdb78def6b87c892a2
Kacper <kacper.gajko1@gmail.com>
2024-07-24 16:01:22 +0200

    server with socket io integration

--- server.js
+++ server.js
@@ -1,20 +1,29 @@
-const express = require("express");
-const next = require("next");
+import { createServer } from "node:http";
+import next from "next";
+import { Server } from "socket.io";
 
-const port = parseInt(process.env.PORT, 10) || 3000;
 const dev = process.env.NODE_ENV !== "production";
-const app = next({ dev });
-const handle = app.getRequestHandler();
+const hostname = "localhost";
+const port = 3000;
+// when using middleware `hostname` and `port` must be provided below
+const app = next({ dev, hostname, port });
+const handler = app.getRequestHandler();
 
 app.prepare().then(() => {
-	const server = express();
+	const httpServer = createServer(handler);
 
-	server.all("*", (req, res) => {
-		return handle(req, res);
+	const io = new Server(httpServer);
+
+	io.on("connection", (socket) => {
+		console.log("User connected");
 	});
 
-	server.listen(port, (err) => {
-		if (err) throw err;
-		console.log(`> Ready on http://localhost:${port}`);
-	});
+	httpServer
+		.once("error", (err) => {
+			console.error(err);
+			process.exit(1);
+		})
+		.listen(port, () => {
+			console.log(`> Ready on http://${hostname}:${port}`);
+		});
 });
