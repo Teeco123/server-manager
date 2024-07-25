8499fc227a8e46ded264bc161eb2b6d1cc97c9b1
Kacper <kacper.gajko1@gmail.com>
2024-07-25 19:26:55 +0200

    added more navtiles

--- app/components/navbar/navbar.tsx
+++ app/components/navbar/navbar.tsx
@@ -28,6 +28,24 @@ export default function Navbar() {
 				<img src='overview.png' />
 				<div className='tile-name'>Overview</div>
 			</a>
+			<a
+				href='/usage'
+				className='nav-tile'>
+				<img src='usage.png' />
+				<div className='tile-name'>Usage</div>
+			</a>
+			<a
+				href='/photos'
+				className='nav-tile'>
+				<img src='photos.png' />
+				<div className='tile-name'>Photos</div>
+			</a>
+			<a
+				href='/docker'
+				className='nav-tile'>
+				<img src='docker.png' />
+				<div className='tile-name'>Docker</div>
+			</a>
 		</div>
 	);
 }
