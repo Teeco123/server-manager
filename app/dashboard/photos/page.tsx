a576a06312e0a738c4033281925f8abfc4856824
Kacper <kacper.gajko1@gmail.com>
2024-07-26 22:16:16 +0200

    styling of buttons

--- app/dashboard/photos/page.tsx
+++ app/dashboard/photos/page.tsx
@@ -7,12 +7,19 @@ export default function Photos() {
 		<div className='main-panel'>
 			<div className='upload'>
 				<form action={UploadFile}>
-					<input
-						name='files'
-						type='file'
-						multiple
-					/>
-					<button type='submit'></button>
+					<label className='input-label'>
+						<img src='/upload file.png' />
+						<input
+							className='file-input'
+							name='files'
+							type='file'
+							multiple></input>
+					</label>
+
+					<button type='submit'>
+						<img src='/upload.png' />
+						SUBMIT
+					</button>
 				</form>
 			</div>
 		</div>
